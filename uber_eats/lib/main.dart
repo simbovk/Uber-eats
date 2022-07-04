9525f3c3a626cea0800a78de1bab05a538fd981c
Amirali Vakili <amirv1382@gmail.com>
2022-07-04 14:57:20 +0430

    now can scroll the page

--- uber_eats/lib/main.dart
+++ uber_eats/lib/main.dart
@@ -19,126 +19,128 @@ class _SignUpState extends State<SignUp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          children: [
-            Center(
-              child: Container(
-                height: 100,
-                width: 250,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      opacity: 0.6,
-                      image: AssetImage("assets/images/uber_eats_logo.png"),
-                      fit: BoxFit.fill),
-                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  height: 100,
+                  width: 250,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        opacity: 0.6,
+                        image: AssetImage("assets/images/uber_eats_logo.png"),
+                        fit: BoxFit.fill),
+                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            Center(
-              child: Container(
-                width: 300,
-                child: TextField(
-                  showCursor: false,
-                  textAlign: TextAlign.center,
-                  cursorColor: Colors.black.withOpacity(0.8),
-                  decoration: InputDecoration(
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.greenAccent[400]!)),
-                      iconColor: Colors.green,
-                      hintStyle: const TextStyle(color: Colors.grey),
-                      hintText: "name"),
+              SizedBox(height: 10),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: TextField(
+                    showCursor: false,
+                    textAlign: TextAlign.center,
+                    cursorColor: Colors.black.withOpacity(0.8),
+                    decoration: InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.greenAccent[400]!)),
+                        iconColor: Colors.green,
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        hintText: "name"),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            Center(
-              child: Container(
-                width: 300,
-                child: TextField(
-                  showCursor: false,
-                  textAlign: TextAlign.center,
-                  cursorColor: Colors.black.withOpacity(0.8),
-                  decoration: InputDecoration(
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.greenAccent[400]!)),
-                      iconColor: Colors.green,
-                      hintStyle: const TextStyle(color: Colors.grey),
-                      hintText: "email"),
+              SizedBox(height: 20),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: TextField(
+                    showCursor: false,
+                    textAlign: TextAlign.center,
+                    cursorColor: Colors.black.withOpacity(0.8),
+                    decoration: InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.greenAccent[400]!)),
+                        iconColor: Colors.green,
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        hintText: "email"),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            Center(
-              child: Container(
-                width: 300,
-                child: TextField(
-                  showCursor: false,
-                  textAlign: TextAlign.center,
-                  cursorColor: Colors.black.withOpacity(0.8),
-                  decoration: InputDecoration(
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.greenAccent[400]!)),
-                      iconColor: Colors.green,
-                      hintStyle: const TextStyle(color: Colors.grey),
-                      hintText: "phonenumber"),
+              SizedBox(height: 20),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: TextField(
+                    showCursor: false,
+                    textAlign: TextAlign.center,
+                    cursorColor: Colors.black.withOpacity(0.8),
+                    decoration: InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.greenAccent[400]!)),
+                        iconColor: Colors.green,
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        hintText: "phonenumber"),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            Center(
-              child: Container(
-                width: 300,
-                child: TextField(
-                  showCursor: false,
-                  obscureText: showPass,
-                  textAlign: TextAlign.center,
-                  cursorColor: Colors.black.withOpacity(0.8),
-                  decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              showPass = !showPass;
-                            });
-                          },
-                          icon: Icon(Icons.remove_red_eye),
-                          color: Colors.green),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.greenAccent[400]!)),
-                      iconColor: Colors.green,
-                      hintStyle: const TextStyle(color: Colors.grey),
-                      hintText: "password"),
+              SizedBox(height: 20),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: TextField(
+                    showCursor: false,
+                    obscureText: showPass,
+                    textAlign: TextAlign.center,
+                    cursorColor: Colors.black.withOpacity(0.8),
+                    decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                showPass = !showPass;
+                              });
+                            },
+                            icon: Icon(Icons.remove_red_eye),
+                            color: Colors.green),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.greenAccent[400]!)),
+                        iconColor: Colors.green,
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        hintText: "password"),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            SizedBox(
-              height: 30,
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () {
-                  // sendInfoToServer(
-                  //     _controllerFirstName.text,
-                  //     _controllerLastName.text,
-                  //     _controllerPassword.text,
-                  //     _controllerPhoneNumber.text,
-                  //     _controllerEmail.text);
-                },
-                child: const Text('Sign up'),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.green),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0),
-                          side: const BorderSide(color: Colors.green))),
+              SizedBox(height: 20),
+              SizedBox(
+                height: 30,
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // sendInfoToServer(
+                    //     _controllerFirstName.text,
+                    //     _controllerLastName.text,
+                    //     _controllerPassword.text,
+                    //     _controllerPhoneNumber.text,
+                    //     _controllerEmail.text);
+                  },
+                  child: const Text('Sign up'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.green),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: const BorderSide(color: Colors.green))),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
