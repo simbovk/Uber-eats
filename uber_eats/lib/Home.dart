8a582ca312b9541d51e62e1c91eecdbc75173383
Amirali Vakili <amirv1382@gmail.com>
2022-07-05 21:24:48 +0430

    pages updaetd

--- uber_eats/lib/Home.dart
+++ uber_eats/lib/Home.dart
@@ -13,6 +13,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.green,
