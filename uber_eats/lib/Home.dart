c16d573cf26aa60c7c4910dce3d09433c2e82194
Amirali Vakili <amirv1382@gmail.com>
2022-07-06 12:35:42 +0430

    search bar updated

--- uber_eats/lib/Home.dart
+++ uber_eats/lib/Home.dart
@@ -17,116 +17,125 @@ class _HomeState extends State<Home> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(backgroundColor: Colors.green , actions: <Widget>[
-          ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-              surfaceTintColor: Colors.white,
-              primary: Colors.green,
-              onPrimary: Colors.white,
-              shadowColor: Colors.white,
-              shape: RoundedRectangleBorder(
-               borderRadius: new BorderRadius.circular(30.0),
-               ),
+          appBar: AppBar(backgroundColor: Colors.green, actions: <Widget>[
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                surfaceTintColor: Colors.white,
+                primary: Colors.green,
+                onPrimary: Colors.white,
+                shadowColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0),
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => SignUp()));
+              },
+              icon: Icon(
+                Icons.account_circle_outlined,
+                size: 20,
+              ),
+              label: Text('Enter'),
             ),
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => SignUp()));
-            },
-            icon: Icon(
-              Icons.account_circle_outlined,
-              size: 20,
+          ]),
+          drawer: Drawer(
+            child: ListView(
+              children: <Widget>[
+                DrawerHeader(
+                    child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 130),
+                      child: Container(
+                        height: 70,
+                        width: 210,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              opacity: 0.6,
+                              image: AssetImage(
+                                  "assets/images/uber_eats_logo.png"),
+                              fit: BoxFit.fill),
+                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 180),
+                      child: Text(
+                        'Amirali Vakili',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                    )
+                  ],
+                )),
+                ListTile(
+                  title: Text('Home'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                Divider(
+                  color: Colors.green,
+                ),
+                ListTile(
+                  title: Text('About'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                Divider(
+                  color: Colors.green,
+                ),
+                ListTile(
+                  title: Text('Contact'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
             ),
-            label: Text('Enter'),
           ),
-        ]
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              DrawerHeader(
-                  child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 130),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Center(
                     child: Container(
-                      height: 70,
-                      width: 210,
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                            opacity: 0.6,
-                            image:
-                                AssetImage("assets/images/uber_eats_logo.png"),
-                            fit: BoxFit.fill),
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      width: 400,
+                      color: Colors.grey.withOpacity(0.4),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Text(
+                              'Search',
+                              style: TextStyle(
+                                  color: Colors.black.withOpacity(0.6),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                            ),
+                          ),
+                          Spacer(),
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.search,
+                                color: Colors.green,
+                              )),
+                        ],
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 180),
-                    child: Text(
-                      'Amirali Vakili',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-                    ),
-                  )
-                ],
-              )),
-              ListTile(
-                title: Text('Home'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              Divider(
-                color: Colors.green,
-              ),
-              ListTile(
-                title: Text('About'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              Divider(
-                color: Colors.green,
-              ),
-              ListTile(
-                title: Text('Contact'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        ),
-        body:SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: Center(
-                  child: Container(
-                    width: 400,
-                    color: Colors.grey.withOpacity(0.4),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10),
-                          child: Text('Search' , style: TextStyle(color: Colors.green , fontWeight: FontWeight.bold , fontSize: 20),),
-                        ),
-                        Spacer(),
-                        IconButton(onPressed: (){}, icon: Icon(Icons.search , color: Colors.green,)),
-                      ],
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        )
-      ),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
