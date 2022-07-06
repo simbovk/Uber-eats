682ead284c09d2204f138eab09a9eda7dcfdedae
Amirali Vakili <amirv1382@gmail.com>
2022-07-06 18:10:04 +0430

    list view updated

--- uber_eats/lib/Home.dart
+++ uber_eats/lib/Home.dart
@@ -411,6 +411,7 @@ class _HomeState extends State<Home> {
                                 )
                               ],
                             ),
+                            SizedBox(width: 20,)
                         ],
                       ),
                     ),
