6dea43dfab912336619cbfee17597ee7b8f014b8
Amirali Vakili <amirv1382@gmail.com>
2022-07-12 21:24:09 +0430

    update drawer

--- lib/Home.dart
+++ lib/Home.dart
@@ -55,66 +55,71 @@ class _HomeState extends State<Home> {
             label: Text('Enter'),
           ),
         ]),
-        drawer: Drawer(
-          // shape: ShapeBorder(),
-          child: ListView(
-            children: <Widget>[
-              DrawerHeader(
-                  child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 130),
-                    child: Container(
-                      height: 70,
-                      width: 210,
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                            opacity: 0.6,
-                            image:
-                                AssetImage("assets/images/uber_eats_logo.png"),
-                            fit: BoxFit.fill),
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        drawer: ClipRRect(
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(35), bottomRight: Radius.circular(35)),
+          child: Drawer(
+            width: 250,
+            // shape: ShapeBorder(),
+            child: ListView(
+              children: <Widget>[
+                DrawerHeader(
+                    child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 130),
+                      child: Container(
+                        height: 70,
+                        width: 210,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              opacity: 0.6,
+                              image: AssetImage(
+                                  "assets/images/uber_eats_logo.png"),
+                              fit: BoxFit.fill),
+                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 180),
-                    child: Text(
-                      'Amirali Vakili',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                    SizedBox(
+                      height: 40,
                     ),
-                  )
-                ],
-              )),
-              ListTile(
-                title: Text('Home'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              Divider(
-                color: Colors.green,
-              ),
-              ListTile(
-                title: Text('About'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              Divider(
-                color: Colors.green,
-              ),
-              ListTile(
-                title: Text('Contact'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
+                    Padding(
+                      padding: const EdgeInsets.only(right: 180),
+                      child: Text(
+                        'Amirali Vakili',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                    )
+                  ],
+                )),
+                ListTile(
+                  title: Text('Home'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                Divider(
+                  color: Colors.green,
+                ),
+                ListTile(
+                  title: Text('About'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                Divider(
+                  color: Colors.green,
+                ),
+                ListTile(
+                  title: Text('Contact'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
           ),
         ),
         body: SingleChildScrollView(
