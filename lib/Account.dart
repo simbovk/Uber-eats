9a5c2dd8547c2a9bb2e8158dc62b515914f093af
Amirali Vakili <amirv1382@gmail.com>
2022-07-19 23:00:27 +0430

    appbar updated

--- lib/Account.dart
+++ lib/Account.dart
@@ -14,27 +14,46 @@ class _MyAccountState extends State<MyAccount> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-        title: Center(child: Text('Round AppBar')),
-        backgroundColor: Colors.lightGreen,
-        elevation: 0,
-        actions: [Icon(Icons.dehaze_sharp)],
-      ),
-      body: SafeArea(
-          child: Column(
-        children: [
-          ClipPath(
-            clipper: RoundShape(),
-            child: Container(
-              height: 40,
-              color: Colors.lightGreen,
+        appBar: AppBar(
+          title: Center(child: Text('Round AppBar')),
+          backgroundColor: Colors.lightGreen,
+          elevation: 0,
+          actions: [Icon(Icons.dehaze_sharp)],
+        ),
+        body: SafeArea(
+            child: Column(
+          children: [
+            ClipPath(
+              clipper: RoundShape(),
+              child: Container(
+                height: 40,
+                color: Colors.lightGreen,
+              ),
             ),
-          ),
-          Center(
-            child: Text('Body'),
-          ),
-        ],
-      )),),
+            Center(
+              child: Text('Body'),
+            ),
+          ],
+        )),
+      ),
     );
   }
+}
+
+class RoundShape extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    double height = size.height;
+    double width = size.width;
+    double curveHeight = size.height / 2;
+    var p = Path();
+    p.lineTo(0, height - curveHeight);
+    p.quadraticBezierTo(width / 2, height, width, height - curveHeight);
+    p.lineTo(width, 0);
+    p.close();
+    return p;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) => true;
 }
