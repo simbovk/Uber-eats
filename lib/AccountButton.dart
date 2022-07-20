59835a5f58ae1f98bd0254ee1e3910298bfd1860
Amirali Vakili <amirv1382@gmail.com>
2022-07-20 11:31:50 +0430

    account button info updated

--- lib/AccountButton.dart
+++ lib/AccountButton.dart
@@ -13,6 +13,40 @@ class _AccountButtonState extends State<AccountButton> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar:  AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          actions: [
+            Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      padding: const EdgeInsets.only(right: 330),
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        color: Colors.green,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    IconButton(
+                      // padding: ,
+                      icon: const Icon(
+                        Icons.shopping_basket_rounded,
+                        color: Colors.green,
+                      ),
+                      onPressed: () {
+                        // like drawer
+                      },
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
         body: info(),
       ),
     );
