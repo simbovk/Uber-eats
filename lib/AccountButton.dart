b90e674cacad92ddb44d7ab3f9b756e01c171eb1
Amirali Vakili <amirv1382@gmail.com>
2022-07-20 11:41:59 +0430

    account button info updated

--- lib/AccountButton.dart
+++ lib/AccountButton.dart
@@ -13,7 +13,7 @@ class _AccountButtonState extends State<AccountButton> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar:  AppBar(
+        appBar: AppBar(
           backgroundColor: Colors.white,
           elevation: 0,
           actions: [
@@ -56,105 +56,86 @@ class _AccountButtonState extends State<AccountButton> {
 Widget info() {
   return Column(
     children: [
+      SizedBox(height: 10),
       Center(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 100),
-                  child: Container(
-                    height: 100,
-                    width: 250,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          opacity: 0.6,
-                          image: AssetImage("assets/images/uber_eats_logo.png"),
-                          fit: BoxFit.fill),
-                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              Center(
-                child: Container(
-                  width: 300,
-                  child: TextField(
-                    // showCursor: false,
-                    textAlign: TextAlign.center,
-                    cursorColor: Colors.black.withOpacity(0.8),
-                    decoration: InputDecoration(
-                        focusedBorder: UnderlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.greenAccent[400]!)),
-                        iconColor: Colors.green,
-                        hintStyle: const TextStyle(color: Colors.grey),
-                        hintText: "name"),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Center(
-                child: Container(
-                  width: 300,
-                  child: TextField(
-                    // showCursor: false,
-                    textAlign: TextAlign.center,
-                    cursorColor: Colors.black.withOpacity(0.8),
-                    decoration: InputDecoration(
-                        focusedBorder: UnderlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.greenAccent[400]!)),
-                        iconColor: Colors.green,
-                        hintStyle: const TextStyle(color: Colors.grey),
-                        hintText: "email"),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Center(
-                child: Container(
-                  width: 300,
-                  child: TextField(
-                    // showCursor: false,
-                    textAlign: TextAlign.center,
-                    cursorColor: Colors.black.withOpacity(0.8),
-                    decoration: InputDecoration(
-                        focusedBorder: UnderlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.greenAccent[400]!)),
-                        iconColor: Colors.green,
-                        hintStyle: const TextStyle(color: Colors.grey),
-                        hintText: "phonenumber"),
-                  ),
-                ),
-              ),
-              SizedBox(height: 40),
-              SizedBox(
-                height: 30,
-                width: 100,
-                child: ElevatedButton(
-                  // sendInfoToServer(
-                  //     _controllerFirstName.text,
-                  //     _controllerLastName.text,
-                  //     _controllerPassword.text,
-                  //     _controllerPhoneNumber.text,
-                  //     _controllerEmail.text);
-                  child: const Text('Submit'),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.green),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: const BorderSide(color: Colors.green))),
-                  ),
-                  onPressed: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (context) => const Home(),
-                    //   ),
-                    // );
-                  },
-                ),
-              ),
+        child: Container(
+          width: 300,
+          child: TextFormField(
+            initialValue: 'Amirali Vakili',
+            textAlign: TextAlign.center,
+            cursorColor: Colors.black.withOpacity(0.8),
+             decoration: InputDecoration(
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.greenAccent[400]!)),
+                iconColor: Colors.green,
+                hintStyle: const TextStyle(color: Colors.grey),
+                hintText: "name"),
+          )
+        ),
+      ),
+      SizedBox(height: 20),
+      Center(
+        child: Container(
+          width: 300,
+          child: TextField(
+            // showCursor: false,
+            textAlign: TextAlign.center,
+            cursorColor: Colors.black.withOpacity(0.8),
+            decoration: InputDecoration(
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.greenAccent[400]!)),
+                iconColor: Colors.green,
+                hintStyle: const TextStyle(color: Colors.grey),
+                hintText: "email"),
+          ),
+        ),
+      ),
+      SizedBox(height: 20),
+      Center(
+        child: Container(
+          width: 300,
+          child: TextField(
+            // showCursor: false,
+            textAlign: TextAlign.center,
+            cursorColor: Colors.black.withOpacity(0.8),
+            decoration: InputDecoration(
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.greenAccent[400]!)),
+                iconColor: Colors.green,
+                hintStyle: const TextStyle(color: Colors.grey),
+                hintText: "phonenumber"),
+          ),
+        ),
+      ),
+      SizedBox(height: 40),
+      SizedBox(
+        height: 30,
+        width: 100,
+        child: ElevatedButton(
+          // sendInfoToServer(
+          //     _controllerFirstName.text,
+          //     _controllerLastName.text,
+          //     _controllerPassword.text,
+          //     _controllerPhoneNumber.text,
+          //     _controllerEmail.text);
+          child: const Text('Submit'),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.green),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    side: const BorderSide(color: Colors.green))),
+          ),
+          onPressed: () {
+            // Navigator.push(
+            //   context,
+            //   MaterialPageRoute(
+            //     builder: (context) => const Home(),
+            //   ),
+            // );
+          },
+        ),
+      ),
     ],
   );
 }
