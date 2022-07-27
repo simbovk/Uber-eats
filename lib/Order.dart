f720e33e00862a7f514b5a4d2c734f375f845000
Amirali Vakili <amirv1382@gmail.com>
2022-07-27 19:36:51 +0430

    Orders updated

--- lib/Order.dart
+++ lib/Order.dart
@@ -54,123 +54,117 @@ class _OrderState extends State<Order> {
     this.imagePath = imagePath;
     this.name = name;
     this.deliverd = deliverd;
+    this.date = date;
+    this.month = month;
+    this.watch = watch;
   }
   @override
   Widget build(BuildContext context) {
+    String deliverdTime = '$date,$month  $watch';
     String deliverdText = deliverd == true ? 'Delivered' : 'Not Delivered';
-    return GestureDetector(
-      onTap: () {
-        // Navigator.push(
-        //               context,
-        //               MaterialPageRoute(
-        //                 builder: (context) => const Home(),
-        //               ),
-        //             );
-      },
-      child: Padding(
-        padding: const EdgeInsets.only(left: 15),
-        child: Container(
-          height: 190,
-          width: MediaQuery.of(context).size.width - 50,
-          decoration: BoxDecoration(
-            color: Colors.green.withOpacity(0.2),
-            borderRadius: BorderRadius.all(Radius.circular(12.0)),
-          ),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10, left: 0),
-                    child: Column(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage(imagePath), fit: BoxFit.fill),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(12.0)),
-                          ),
-                          height: 120,
-                          width: 120,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Column(
+    return Padding(
+      padding: const EdgeInsets.only(left: 15),
+      child: Container(
+        height: 190,
+        width: MediaQuery.of(context).size.width - 50,
+        decoration: BoxDecoration(
+          color: Colors.green.withOpacity(0.2),
+          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 0),
+                  child: Column(
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 25),
-                        child: Row(
-                          children: [
-                            Text(
-                              name,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Colors.black),
-                            ),
-                            SizedBox(
-                              width: 55,
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                color: Colors.green.withOpacity(0.2),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(32.0)),
-                              ),
-                              child: Text(
-                                deliverdText != null ? deliverdText : '',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 10,
-                                    color: Colors.green),
-                              ),
-                            ),
-                          ],
+                      Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage(imagePath), fit: BoxFit.fill),
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
                         ),
+                        height: 120,
+                        width: 120,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 8, right: 60),
-                        child: Text(
-                            '200g meat,regular Bun,\nketchup,pickle Slices,\nonions and mustard '),
-                      )
                     ],
                   ),
-                ],
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Divider(color: Colors.black.withOpacity(0.3)),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Text(
-                      resturantName,
-                      style: TextStyle(
-                          color: Colors.black.withOpacity(0.7),
-                          fontWeight: FontWeight.bold,
-                          fontStyle: FontStyle.italic),
+                ),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 25),
+                      child: Row(
+                        children: [
+                          Text(
+                            name,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: Colors.black),
+                          ),
+                          SizedBox(
+                            width: 55,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.green.withOpacity(0.2),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            child: Text(
+                              deliverdText != null ? deliverdText : '',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10,
+                                  color: Colors.green),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8, right: 60),
+                      child: Text(
+                        deliverdTime != null ? deliverdTime:''
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Divider(color: Colors.black.withOpacity(0.3)),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Text(
+                    resturantName,
+                    style: TextStyle(
+                        color: Colors.black.withOpacity(0.7),
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic),
                   ),
-                  Spacer(),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: Text(
-                      r'$' + price,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                          fontStyle: FontStyle.italic),
-                    ),
+                ),
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Text(
+                    r'$' + price,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontStyle: FontStyle.italic),
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
