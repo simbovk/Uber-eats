728ac84daff42366cdb68223f715d6e961956d8f
Amirali Vakili <amirv1382@gmail.com>
2022-07-30 17:02:49 +0430

    search bar updated

---
+++ lib/SearchBar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class MySearchDelegate extends SearchDelegate {
+  List<String> suggestion = ['food', 'supermarket', 'resturant'];
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            if (query.isEmpty) {
+              close(context, null);
+            } else {
+              query = '';
+            }
+          },
+          icon: Icon(
+            Icons.clear_rounded,
+            color: Colors.green,
+          ))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () => close(context, null),
+        icon: Icon(
+          Icons.arrow_back,
+          color: Colors.green,
+        ));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Center(
+      child: Text(
+        query,
+        style: TextStyle(fontSize: 20, color: Colors.black),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestions = suggestion.where((suggest) {
+      final result = suggest.toLowerCase();
+      final input = query.toLowerCase();
+      return result.contains(input);
+    }).toList();
+    return ListView.builder(
+        itemCount: suggestions.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(suggestions[index]),
+            onTap: () {
+              query = suggestions[index];
+            },
+          );
+        });
+  }
+}
