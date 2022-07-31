d7a48079068ce456783aa07f9b2ea7a7f6d59920
Amirali Vakili <amirv1382@gmail.com>
2022-07-31 19:04:39 +0430

    resturant info builded

--- lib/Categories.dart
+++ lib/Categories.dart
@@ -66,10 +66,10 @@ class _CategoriesState extends State<Categories> {
                 ),
                 Spacer(),
                 Padding(
-                  padding: const EdgeInsets.only(right: 15),
+                  padding: const EdgeInsets.only(right:5),
                   child: Container(
                     height: 25,
-                    width: 150,
+                    width: 160,
                     child: DropdownButton(
                       hint: Text(
                         'According to...',
@@ -87,54 +87,51 @@ class _CategoriesState extends State<Categories> {
                         DropdownMenuItem(
                           child: Container(
                               height: 25,
-                              width: 75,
-                              child: Center(child: const Text("Most rated" ,style: TextStyle(), ))),
+                              width: 120,
+                              child: Center(child: const Text("Most rated" ,style: TextStyle(color: Colors.black), ))),
                           value: 'Most Rated',
                         ),
                         DropdownMenuItem(
                           child: Container(
                               height: 25,
-                              width: 75,
-                              color: Colors.white,
-                              child: Center(
-                                child: const Text(
-                                  "white",
-                                  style: TextStyle(color: Colors.black),
-                                ),
-                              )),
-                          value: 'white',
+                              width: 120,
+                              child: Center(child: const Text("Most ordered" ,style: TextStyle(color: Colors.black), ))),
+                          value: 'Most Ordered',
                         ),
                         DropdownMenuItem(
                           child: Container(
                               height: 25,
-                              width: 75,
-                              color: Color.fromARGB(255, 4, 36, 52),
-                              child: Center(child: const Text("blue"))),
-                          value: 'blue',
+                              width: 130,
+                              child: Center(child: const Text("Most expensive" ,style: TextStyle(color: Colors.black), ))),
+                          value: 'Most Expensive',
                         ),
                         DropdownMenuItem(
                           child: Container(
                               height: 25,
-                              width: 75,
-                              color: const Color.fromARGB(255, 207, 37, 25),
-                              child: Center(child: const Text("red"))),
-                          value: 'red',
+                              width: 130,
+                              child: Center(child: const Text("Least expensive" ,style: TextStyle(color: Colors.black), ))),
+                          value: 'Least Expensive',
                         ),
-                        DropdownMenuItem(
+                         DropdownMenuItem(
                           child: Container(
                               height: 25,
-                              width: 75,
-                              color: const Color.fromARGB(255, 255, 122, 166),
-                              child: Center(child: const Text("pink"))),
-                          value: 'pink',
+                              width: 120,
+                              child: Center(child: const Text("Most popular" ,style: TextStyle(color: Colors.black), ))),
+                          value: 'Most popular',
                         ),
-                        DropdownMenuItem(
+                         DropdownMenuItem(
                           child: Container(
                               height: 25,
-                              width: 75,
-                              color: const Color.fromARGB(255, 1, 46, 18),
-                              child: Center(child: const Text("green"))),
-                          value: 'green',
+                              width: 120,
+                              child: Center(child: const Text("newest" ,style: TextStyle(color: Colors.black), ))),
+                          value: 'Newest',
+                        ),
+                         DropdownMenuItem(
+                          child: Container(
+                              height: 25,
+                              width: 120,
+                              child: Center(child: const Text("closest" ,style: TextStyle(color: Colors.black), ))),
+                          value: 'Closest',
                         ),
                       ],
                       onChanged: (String? newValue) {
---
+++ lib/ResturantInfo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ResturantInfo extends StatefulWidget {
+  const ResturantInfo({Key? key}) : super(key: key);
+
+  @override
+  State<ResturantInfo> createState() => _ResturantInfoState();
+}
+
+class _ResturantInfoState extends State<ResturantInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
