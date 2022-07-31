2a3e31d69f85aa7be8d8e785b09a14312ce0d23f
Amirali Vakili <amirv1382@gmail.com>
2022-08-01 00:01:04 +0430

    resturant info page updated

--- lib/ResturantInfo.dart
+++ lib/ResturantInfo.dart
@@ -24,18 +24,20 @@ class _ResturantInfoState extends State<ResturantInfo> {
   late String rate;
   late String resturantImagePath;
   late String resturantLabelImagePath;
+  late String branch;
   _ResturantInfoState(String resturantName, String rate,
-      String resturantImagePath, String resturantLabelImagePath) {
+      String resturantImagePath, String resturantLabelImagePath , String branch) {
     this.resturantName = resturantName;
     this.rate = rate;
     this.resturantImagePath = resturantImagePath;
     this.resturantLabelImagePath = resturantLabelImagePath;
+    this.branch = branch;
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 190,
+      height: 250,
       width: MediaQuery.of(context).size.width - 50,
       decoration: BoxDecoration(
         color: Colors.green.withOpacity(0.2),
@@ -46,12 +48,25 @@ class _ResturantInfoState extends State<ResturantInfo> {
           Container(
             decoration: BoxDecoration(
               image: DecorationImage(
-                  image: AssetImage(resturantImagePath != null ? resturantImagePath : ''), fit: BoxFit.fill),
-              borderRadius: BorderRadius.only(topRight: Radius.circular(12.0), topLeft: Radius.circular(12.0)),
+                  image: AssetImage(
+                      resturantImagePath != null ? resturantImagePath : ''),
+                  fit: BoxFit.fill),
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(12.0),
+                  topLeft: Radius.circular(12.0)),
             ),
-            height: 120,
-            width:362,
+            height: 150,
+            width: 362,
           ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Text(
+                resturantName,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ],
+          )
         ],
       ),
     );
