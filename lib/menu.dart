bde7349093448a8ad0d2cabbf7c56d2f6c4840c5
Amirali Vakili <amirv1382@gmail.com>
2022-08-14 20:25:02 +0430

    menu page updaetd

--- lib/menu.dart
+++ lib/menu.dart
@@ -11,6 +11,7 @@ class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
