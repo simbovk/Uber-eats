4d3a2738a43b72fdfbd01f20f1a0a92ac25246ca
Amirali Vakili <amirv1382@gmail.com>
2022-08-14 20:12:50 +0430

    menu page updaetd

--- lib/menu.dart
+++ lib/menu.dart
@@ -10,6 +10,89 @@ class Menu extends StatefulWidget {
 class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          actions: [
+            Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      padding: const EdgeInsets.only(right: 330),
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        color: Colors.green,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    IconButton(
+                      // padding: ,
+                      icon: const Icon(
+                        Icons.shopping_basket_rounded,
+                        color: Colors.green,
+                      ),
+                      onPressed: () {
+                        // like drawer
+                      },
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                  child: Text(
+                'Menu',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              )),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                  child: Text(
+                'Categories',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              )),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                  child: Text(
+                'Burgers',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              )),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                  child: Text(
+                'Pizza',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              )),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                  child: Text(
+                'Pasta',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              )),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
